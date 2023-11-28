62cfdd2f6defd2b05d6937d8232f4a52f9cba672
jcastroisep <1210816@isep.ipp.pt>
2023-11-28 23:51:50 +0000

    Updating building  SEM5-53
    Updating building component.spec.ts

--- frontend/angular-app/src/app/component/building/building.component.spec.ts
+++ frontend/angular-app/src/app/component/building/building.component.spec.ts
@@ -1,23 +1,44 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
 
 import { BuildingComponent } from './building.component';
+import { BuildingService } from '../../services/building.service';
+import { IBuilding } from "../../models/ibuilding.model";
 
 describe('BuildingComponent', () => {
   let component: BuildingComponent;
   let fixture: ComponentFixture<BuildingComponent>;
+  let buildingService: BuildingService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BuildingComponent]
+      imports: [HttpClientTestingModule],
+      declarations: [BuildingComponent],
+      providers: [BuildingService]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(BuildingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    buildingService = TestBed.inject(BuildingService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should filter buildings', () => {
+    const buildings: IBuilding[] = [
+      { buildingId: '1', buildingName: 'Building 1', buildingDescription: '', buildingNumberOfFloors: 0, dimX: 0, dimY: 0, wallHeight: 0, wallWidth: 0 },
+      { buildingId: '2', buildingName: 'Building 2', buildingDescription: '', buildingNumberOfFloors: 0, dimX: 0, dimY: 0, wallHeight: 0, wallWidth: 0 }
+    ];
+    component.buildings = buildings;
+    component.filterText = '1';
+    component.filterBuildings();
+    expect(component.filteredBuildings.length).toBe(1);
+    expect(component.filteredBuildings[0].buildingId).toBe('1');
+  });
+
+  // Add more tests for other methods and properties
 });
